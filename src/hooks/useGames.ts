5c4039ed4f41161b22c583b7dc764b016d72f7a0
the3musketeersgh <the3musketeersgh@gmail.com>
2024-08-26 17:29:56 +0000

    refactor: Extract the query object

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,6 +3,7 @@ import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 import useData from "./useData";
 import { Genre } from "./useGenres";
+import { GameQuery } from "../App";
 
 export interface Platform {
   id: number;
@@ -21,5 +22,8 @@ interface FetchGameResponse {
   count: number;
   results: Game[];
 }
-const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null) => useData<Game>("/games", {params: {genres: selectedGenre?.id, platforms: selectedPlatform?.id}}, [selectedGenre?.id, selectedPlatform?.id]);
+const useGames = (
+  gameQuery: GameQuery
+) => useData<Game>("/games", {params: {genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id}}, [gameQuery]);
+
 export default useGames;
