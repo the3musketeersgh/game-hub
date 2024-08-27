5c19ec3a314a697dc8c3d7441755b590075e8f5c
the3musketeersgh <the3musketeersgh@gmail.com>
2024-08-27 03:40:10 +0000

    clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -3,6 +3,7 @@ import useGenres, { Genre } from "../hooks/useGenres";
 import useData from "../hooks/useData";
 import {
   Button,
+  Heading,
   HStack,
   Image,
   List,
@@ -23,16 +24,21 @@ function GenreList({ selectedGenre, onSelectGenre }: Props) {
   if (isLoading) return <Spinner />;
 
   return (
+    <>
+    <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
     <List>
       {data.map((genre) => (
         <ListItem key={genre.id} paddingY="5px">
           <HStack>
             <Image
+              objectFit="cover"
               boxSize="32px"
               borderRadius={8}
               src={getCroppedImageUrl(genre.image_background)}
             />
             <Button
+              whiteSpace={"normal"}
+              textAlign="left"
               fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
               onClick={() => onSelectGenre(genre)}
               fontSize="lg"
@@ -44,6 +50,7 @@ function GenreList({ selectedGenre, onSelectGenre }: Props) {
         </ListItem>
       ))}
     </List>
+    </>
   );
 }
 
